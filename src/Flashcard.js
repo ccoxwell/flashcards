de78164ce2a1981748b2015ebf4d68f07faac03b
Carrie Coxwell <carrie.coxwell@gmail.com>
2017-08-09 16:13:58 -0500

    created flashcard and centered it properly

---
+++ src/Flashcard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Flashcard.css'
+
+class Flashcard extends Component {
+  render() {
+    return (
+      <div className="flashcard">
+        <div className="question">
+            <h1>hello peppercorn</h1>
+            <p>
+              content asdf fdsa effwasdf content asdf fdsa 
+              effwasdf content asdf fdsa effwasdf content asdf 
+              fdsa effwasdf content asdf fdsa effwasdf content 
+              asdf fdsa effwasdf 
+            </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Flashcard
