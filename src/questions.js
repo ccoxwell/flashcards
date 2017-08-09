8d046449038b9d5ffcf41bebe0193ff67695cbb2
Carrie Coxwell <carrie.coxwell@gmail.com>
2017-08-09 18:17:51 -0500

    created sample questions, minor css changes

---
+++ src/questions.js
@@ -0,0 +1,17 @@
+[
+  {
+    "question" : "What is the difference between a class an an id?",
+    "answer" : "A class can be used on multiple elements, while an id can only be used on one.",
+    "mastered" : false
+  },
+  {
+    "question" : "What is the difference between an <ol> and a <ul>?",
+    "answer" : "A ul is an unordered list that produces bullets and an ol is an ordered list that produces numbers",
+    "mastered" : false
+  },
+  {
+    "question" : "How do you start a for loop?",
+    "answer" : "<pre>for (var i = 0; i < 0; i++)</pre>",
+    "mastered" : false
+  }
+]
